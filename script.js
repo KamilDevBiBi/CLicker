cc168de12eedb70fdd76758e03ded29949e3d5d3
Kamil <kamil_rr1@mail.ru>
2026-07-15 14:29:15 +0300

    add: reset progress mechanics

--- script.js
+++ script.js
@@ -547,3 +547,8 @@ function updateBrawlerCardUI(){
         }
     }
 }
+
+if (window.location.search.includes("reset=true")) {
+    localStorage.removeItem("brawl_stars_clicker_save");
+    window.location.href = window.location.origin + window.location.pathname; // очищаем ссылку
+}
